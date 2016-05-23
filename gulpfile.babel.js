0599b263375f98f69cab5aba335bc1edc29774d7
AmmarCSE <ammar.abu-yasein@farsightsoftware.com>
2016-05-23 20:17:27 +0300

    setup boilerplate

---
+++ gulpfile.babel.js
@@ -0,0 +1,39 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import webpack from 'webpack-stream';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+gulp.task('default', () => {
+  return gulp.src('src/grid/App.js')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('scripts', () => {
+  return gulp.src('src/grid/App.js')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('dist/'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('serve', [ 'scripts'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch([
+    'index.html'
+  ]).on('change', reload);
+
+  gulp.watch('src/**/*.js', ['scripts']);
+});
+
---
+++ src/App.js
@@ -0,0 +1,3 @@
+const App = true;
+
+export default App
